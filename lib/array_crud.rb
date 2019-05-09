486dbcffd810a1c7f929934bfe1bdda65dd5843e
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-09 17:19:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [naruto, sakura, sasuka, kakashi]
+ anime = [naruto, sakura, sasuka, kakashi]
 end
 
 def add_element_to_end_of_array(array, element)
